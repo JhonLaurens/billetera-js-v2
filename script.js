3f8e3444627e7ef72139f03560e84f463cc0ea23
jhonlaurens <jhonlaurens@gmail.com>
2024-09-01 21:42:10 -0500

    updata

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ let currentAccount = null;
 
 // Conectar a Ganache al cargar la página
 window.addEventListener('load', async () => {
-    // Conectar a Ganache usando Web3.js
     web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
     try {
         if (await web3.eth.net.isListening()) {
             console.log("Conectado a Ganache en http://127.0.0.1:7545");
-            // Mostrar el menú principal
             document.getElementById('menu').style.display = 'block';
         } else {
             alert('Error al conectar a Ganache. Asegúrate de que Ganache esté en ejecución.');
@@ -19,52 +17,91 @@ window.addEventListener('load', async () => {
     }
 });
 
-// Función para agregar un log a la consola
-function log(message) {
-    const output = document.getElementById('output');
-    const logEntry = document.createElement('div');
-    logEntry.textContent = message;
-    output.appendChild(logEntry);
-}
-
 // Mostrar el formulario para importar la cuenta
 function mostrarImportar() {
-    // Ocultar otros formularios y mostrar el formulario de importación
     document.getElementById('menu').style.display = 'none';
     document.getElementById('importForm').style.display = 'block';
     document.getElementById('sendForm').style.display = 'none';
     document.getElementById('accountInfo').style.display = 'none';
+    document.getElementById('blockExplorer').style.display = 'none';
     document.getElementById('backBtn').style.display = 'block';
-
-    log("Mostrando formulario de importación de cuenta.");
 }
 
 // Mostrar el formulario para enviar ETH
 function mostrarEnviarETH() {
-    // Ocultar otros formularios y mostrar el formulario de envío de ETH
     document.getElementById('menu').style.display = 'none';
     document.getElementById('sendForm').style.display = 'block';
     document.getElementById('importForm').style.display = 'none';
     document.getElementById('accountInfo').style.display = 'none';
+    document.getElementById('blockExplorer').style.display = 'none';
     document.getElementById('backBtn').style.display = 'block';
-
-    log("Mostrando formulario de envío de ETH.");
 }
 
+// Mostrar el menú principal
+function volverAlMenu() {
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('importForm').style.display = 'none';
+    document.getElementById('sendForm').style.display = 'none';
+    document.getElementById('accountInfo').style.display = 'none';
+    document.getElementById('blockExplorer').style.display = 'none';
+    document.getElementById('backBtn').style.display = 'none';
+}
+
+// Mostrar el explorador de bloques
+function mostrarExploradorDeBloques() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('blockExplorer').style.display = 'block';
+    document.getElementById('importForm').style.display = 'none';
+    document.getElementById('sendForm').style.display = 'none';
+    document.getElementById('accountInfo').style.display = 'none';
+    document.getElementById('backBtn').style.display = 'block';
+
+    obtenerBloques();
+}
+
+// Obtener información de los bloques
+async function obtenerBloques() {
+    const blocksDiv = document.getElementById('blocks');
+    blocksDiv.innerHTML = '';
+
+    try {
+        const latestBlockNumber = Number(await web3.eth.getBlockNumber());
+        for (let i = latestBlockNumber; i >= 0 && i > latestBlockNumber - 10; i--) {
+            const block = await web3.eth.getBlock(i);
+            const blockElement = document.createElement('div');
+            blockElement.innerHTML = `
+                <h3>Bloque #${block.number}</h3>
+                <p>Minado en: ${new Date(Number(block.timestamp) * 1000).toLocaleString()}</p>
+                <p>Gas Usado: ${block.gasUsed}</p>
+                <p>${block.transactions.length} Transacción(es)</p>
+                <button onclick="mostrarDetalleBloque(${block.number})">Ver Detalles</button>
+            `;
+            blocksDiv.appendChild(blockElement);
+        }
+    } catch (error) {
+        console.error("Error al obtener los bloques:", error);
+        alert(`Error al obtener los bloques. Verifique la conexión. Detalles: ${error.message}`);
+    }
+}
+
+// Consultar saldo de la cuenta importada
 function consultarSaldo() {
     if (currentAccount) {
         web3.eth.getBalance(currentAccount.address)
             .then(balance => {
+                document.getElementById('accountInfo').style.display = 'block';
+                document.getElementById('accountAddress').innerText = `Dirección de la cuenta: ${currentAccount.address}`;
                 document.getElementById('accountBalance').innerText = `Saldo actual: ${web3.utils.fromWei(balance, 'ether')} ETH`;
-                log(`Saldo actual: ${web3.utils.fromWei(balance, 'ether')} ETH`);
             })
             .catch(error => {
                 console.error("Error al consultar el saldo:", error);
-                log("Error al consultar el saldo.");
             });
+    } else {
+        alert("Primero debe importar una cuenta.");
     }
 }
 
+// Enviar transacción
 function enviarTransaccion() {
     const receiverAddress = document.getElementById('receiverAddressInput').value;
     const amount = document.getElementById('amountInput').value;
@@ -74,51 +111,44 @@ function enviarTransaccion() {
         return;
     }
 
-    try {
-        const amountInWei = web3.utils.toWei(amount, 'ether');
+    if (currentAccount) {
+        try {
+            // Obtener el nonce de la cuenta
+            web3.eth.getTransactionCount(currentAccount.address, 'pending')
+                .then(nonce => {
+                    const transaction = {
+                        nonce: nonce,
+                        to: receiverAddress,
+                        value: web3.utils.toWei(amount, 'ether'),
+                        gas: 21000, // Límite de gas
+                        gasPrice: web3.utils.toWei('20', 'gwei'), // Precio del gas
+                        chainId: web3.eth.chainId
+                    };
 
-        web3.eth.getTransactionCount(currentAccount.address)
-            .then(nonce => {
-                const transaction = {
-                    nonce: nonce,
-                    to: receiverAddress,
-                    value: amountInWei,
-                    gas: 21000,
-                    gasPrice: web3.utils.toWei('20', 'gwei'),
-                    chainId: 5777 // Network ID de Ganache
-                };
-
-                return web3.eth.accounts.signTransaction(transaction, currentAccount.privateKey);
-            })
-            .then(signed => {
-                web3.eth.sendSignedTransaction(signed.rawTransaction)
-                    .on('transactionHash', hash => {
-                        document.getElementById('output').innerText = `Transacción enviada con éxito. Hash: ${hash}`;
-                        log(`Transacción enviada con éxito. Hash: ${hash}`);
-                    })
-                    .on('receipt', receipt => {
-                        console.log("Transacción confirmada. Detalles:", receipt);
-                        document.getElementById('output').innerText = `Transacción confirmada en el bloque ${receipt.blockNumber}`;
-                        log(`Transacción confirmada en el bloque ${receipt.blockNumber}`);
-                    })
-                    .on('error', error => {
-                        console.error("Error al enviar la transacción:", error);
-                        alert("Error al enviar la transacción. Verifica la conexión y la cuenta.");
-                        log("Error al enviar la transacción.");
-                    });
-            })
-            .catch(error => {
-                console.error("Error al procesar la transacción:", error);
-                alert("Error al procesar la transacción. Verifica la conexión y la cuenta.");
-                log("Error al procesar la transacción.");
-            });
-    } catch (error) {
-        console.error("Error al convertir cantidad de ETH a Wei:", error);
-        alert("Error al convertir cantidad de ETH a Wei. Intenta de nuevo.");
-        log("Error al convertir cantidad de ETH a Wei.");
+                    web3.eth.accounts.signTransaction(transaction, currentAccount.privateKey)
+                        .then(signedTx => web3.eth.sendSignedTransaction(signedTx.rawTransaction))
+                        .then(receipt => {
+                            alert("ETH enviado exitosamente.");
+                        })
+                        .catch(error => {
+                            console.error("Error al enviar la transacción:", error);
+                            alert("Error al enviar la transacción. Verifique la conexión y la cuenta.");
+                        });
+                })
+                .catch(error => {
+                    console.error("Error al obtener el nonce:", error);
+                    alert("Error al obtener el nonce. Verifique la conexión y la cuenta.");
+                });
+        } catch (error) {
+            alert("Error al enviar la transacción. Verifique la conexión y la cuenta.");
+            console.error("Error al enviar la transacción:", error);
+        }
+    } else {
+        alert("Primero debe importar una cuenta.");
     }
 }
 
+// Importar cuenta
 function importarCuenta() {
     const accountAddress = document.getElementById('accountAddressInput').value;
     const privateKey = document.getElementById('privateKeyInput').value;
@@ -140,29 +170,14 @@ function importarCuenta() {
             privateKey: privateKey
         };
 
-        // Ocultar el formulario de importación y mostrar la información de la cuenta
         document.getElementById('importForm').style.display = 'none';
         document.getElementById('accountInfo').style.display = 'block';
-
-        // Mostrar la dirección de la cuenta
         document.getElementById('accountAddress').innerText = `Dirección de la cuenta: ${currentAccount.address}`;
-
-        // Mostrar el saldo actual
         consultarSaldo();
 
         alert("Cuenta importada correctamente!");
-        log("Cuenta importada correctamente.");
     } catch (error) {
         alert("Clave privada no válida.");
         console.error("Error al importar la cuenta:", error);
-        log("Error al importar la cuenta.");
     }
-}
-
-function volverAlMenu() {
-    document.getElementById('menu').style.display = 'block';
-    document.getElementById('importForm').style.display = 'none';
-    document.getElementById('sendForm').style.display = 'none';
-    document.getElementById('accountInfo').style.display = 'none';
-    document.getElementById('backBtn').style.display = 'none';
 }
